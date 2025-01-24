68f0b210bf6a8d660059944aab93044639eb5892
FlushaM <83796763+FlushaM@users.noreply.github.com>
2025-01-23 23:50:15 -0300

    MODIFCANDO APRA VERCEL

--- CorraleroApp-ValidacionesCorrectas/corralero-backend/server.js
+++ CorraleroApp-ValidacionesCorrectas/corralero-backend/server.js
@@ -46,7 +46,7 @@ app.get('*', (req, res) => {
 });
 
 // Conexión con el puerto
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
